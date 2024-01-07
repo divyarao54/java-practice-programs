89d36067048d299f2b3ff955a049bcdfe325b274
divyarao54 <110017858+divyarao54@users.noreply.github.com>
2024-01-07 20:00:58 +0530

    Add files via upload

---
+++ program6_operators.java
@@ -0,0 +1,28 @@
+public class program6_operators {
+    public static void main(String[] args) {
+
+        // Arithmetic Operators +, -, *, /
+        float a = 10;
+        float b = 20;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+
+        // Relational Operators
+        System.out.println(a < b);
+        System.out.println(a > b);
+        System.out.println(a == b);
+        System.out.println(a != b);
+
+        // Logical Operators AND, OR
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println(x && y);
+        System.out.println(y && x);
+        System.out.println(x || y);
+        System.out.println(y || x);
+    }
+}
