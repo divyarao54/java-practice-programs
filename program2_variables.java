11f5fe1ea8b6a814b3a9e46345c9b4892362ca7b
divyarao54 <110017858+divyarao54@users.noreply.github.com>
2024-01-07 18:10:21 +0530

    Add files via upload

--- program2_variables.java
+++ program2_variables.java
@@ -1,6 +1,12 @@
 public class program2_variables {
     public static void main(String[] args) {
         int x = 10;
+        float y = 3.14f;
+        boolean logic = true;
+        String s = "hello";
         System.out.println(x);
+        System.out.println(y);
+        System.out.println(logic);
+        System.out.println(s);
     }
 }
