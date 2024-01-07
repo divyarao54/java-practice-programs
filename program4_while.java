cd959b3561989617788934b99ae49329c43bb3f7
divyarao54 <110017858+divyarao54@users.noreply.github.com>
2024-01-07 19:28:04 +0530

    Add files via upload

---
+++ program4_while.java
@@ -0,0 +1,9 @@
+public class program4_while {
+    public static void main(String[] args) {
+        int n = 1;
+        while (n <= 4) {
+            System.out.println(n);
+            n++;
+        }
+    }
+}
